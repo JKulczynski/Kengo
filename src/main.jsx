c6325b462fe0d8fa944559f56b7ef9e0ff6bbcab
JKulczynski <j.kulczynski@adform.com>
2026-01-11 13:22:59 +0100

    Fix vercel routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-) 
+  <React.StrictMode>
+    <BrowserRouter> {/* <-- 2. Otwieramy tutaj */}
+      <App />
+    </BrowserRouter> {/* <-- 3. Zamykamy tutaj */}
+  </React.StrictMode>,
+)
